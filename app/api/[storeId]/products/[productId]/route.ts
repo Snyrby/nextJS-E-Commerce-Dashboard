98a16be094bea5563bc658395213ebf1bd69e2c7
Shawn Ruby <103871490+Snyrby@users.noreply.github.com>
2023-07-19 16:28:14 -0700

    Got product form done. Will need to change the way images work

---
+++ app/api/[storeId]/products/[productId]/route.ts
@@ -0,0 +1,174 @@
+import prismadb from "@/lib/prismadb";
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+import { v2 as cloudinary } from "cloudinary";
+cloudinary.config({
+  cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.NEXT_PUBLIC_CLOUDINARY_API_KEY,
+  api_secret: process.env.NEXT_PUBLIC_CLOUDINARY_API_SECRET,
+});
+
+export async function GET(
+  req: Request,
+  { params }: { params: { productId: string } }
+) {
+  try {
+    if (!params.productId) {
+      return new NextResponse("A product Id is required", { status: 400 });
+    }
+    const product = await prismadb.product.findUnique({
+      where: { id: params.productId },
+      include: {
+        images: true,
+        category: true,
+        size: true,
+        color: true,
+      }
+    });
+    return NextResponse.json(product, { status: 200 });
+  } catch (error) {
+    console.log("[PRODUCT_GET]", error);
+    return new NextResponse("Internal error", { status: 500 });
+  }
+}
+
+export async function PATCH(
+  req: Request,
+  { params }: { params: { storeId: string; productId: string } }
+) {
+  try {
+    const { userId } = auth();
+    if (!userId) {
+      return new NextResponse("Unauthenticated", { status: 401 });
+    }
+    const {
+      name,
+      price,
+      categoryId,
+      colorId,
+      sizeId,
+      images,
+      imageId,
+      isFeatured,
+      isArchived,
+    } = await req.json();
+
+    if (!name) {
+      return new NextResponse("Name is required", { status: 400 });
+    }
+
+    if (!images || !images.length) {
+      return new NextResponse("Images are required", { status: 400 });
+    }
+
+    if (!price) {
+      return new NextResponse("Price is required", { status: 400 });
+    }
+
+    if (!categoryId) {
+      return new NextResponse("Category Id is required", { status: 400 });
+    }
+    if (!sizeId) {
+      return new NextResponse("Size Id is required", { status: 400 });
+    }
+    if (!colorId) {
+      return new NextResponse("Color Id is required", { status: 400 });
+    }
+
+    if (!params.productId) {
+      return new NextResponse("A product Id is required", { status: 400 });
+    }
+
+    const storeByUserId = await prismadb.store.findFirst({
+      where: { id: params.storeId, userId },
+    });
+
+    if (!storeByUserId) {
+      return new NextResponse("Unauthorized to perform that action", {
+        status: 403,
+      });
+    }
+    // const oldBillboard = await prismadb.billboard.findFirst({ 
+    //   where: { id: params.billboardId },
+    // })
+    // const oldImageId: any = oldBillboard?.imageId
+    // if (imageId !== oldImageId) {
+    //   await cloudinary.api.delete_resources(oldImageId, {
+    //     type: "upload",
+    //     resource_type: "image",
+    //   });
+    // }
+    await prismadb.product.update({
+      where: { id: params.productId },
+      data: { 
+        name, 
+        price, 
+        categoryId,
+        colorId,
+        sizeId,
+        images: {
+          deleteMany: {}
+        },
+        isFeatured,
+        isArchived, 
+      },
+    });
+    const product = await prismadb.product.update({
+      where: {
+        id: params.productId
+      },
+      data: {
+        images: {
+          createMany: {
+            data: [
+              ...images.map((image: { imageUrl: string }) => image)
+            ]
+          }
+        }
+      }
+    })
+    return NextResponse.json(product, { status: 200 });
+  } catch (error) {
+    console.log("[PRODUCT_PATCH]", error);
+    return new NextResponse("Internal error", { status: 500 });
+  }
+}
+
+export async function DELETE(
+  req: Request,
+  { params }: { params: { storeId: string; productId: string } }
+) {
+  try {
+    const { userId } = auth();
+    if (!userId) {
+      return new NextResponse("Unauthenticated", { status: 401 });
+    }
+    if (!params.productId) {
+      return new NextResponse("A Billboard Id is required", { status: 400 });
+    }
+    const storeByUserId = await prismadb.store.findFirst({
+      where: { id: params.storeId, userId },
+    });
+
+    if (!storeByUserId) {
+      return new NextResponse("Unauthorized to perform that action", {
+        status: 403,
+      });
+    }
+    // const billboard = await prismadb.billboard.findFirst({
+    //   where: { id: params.productId },
+    // });
+    // const imageId: any = billboard?.imageId
+    // await cloudinary.api.delete_resources(imageId, {
+    //   type: "upload",
+    //   resource_type: "image",
+    // });
+    const product = await prismadb.product.deleteMany({
+      where: { id: params.productId },
+    })
+    return NextResponse.json(product, { status: 200 });
+  } catch (error) {
+    console.log("[PRODUCT_DELETE]", error);
+    return new NextResponse("Internal error", { status: 500 });
+  }
+}
